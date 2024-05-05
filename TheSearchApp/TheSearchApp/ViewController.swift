f6857c5d78159642340a42a5acde83c7fe306b77
yasemintk3 <yasemin.tok0702@gmail.com>
2024-05-05 21:42:40 +0300

    theSearchApp

---
+++ TheSearchApp/TheSearchApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheSearchApp
+//
+//  Created by Yasemin TOK on 5.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
