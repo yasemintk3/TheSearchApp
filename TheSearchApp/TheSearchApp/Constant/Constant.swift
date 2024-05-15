b8c4c28ab918b720fc39a7b4ae974832c7daa46b
yasemintk3 <yasemin.tok0702@gmail.com>
2024-05-16 00:18:17 +0300

    refactor

--- TheSearchApp/TheSearchApp/Constant/Constant.swift
+++ TheSearchApp/TheSearchApp/Constant/Constant.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct Constant {
     
-    static let BASE_URL = "https://itunes.apple.com/search?term=jack+johnson."
+    static let BASE_URL = "https://itunes.apple.com/search?"
     static let QUERY_URL = "term="
     
     static let emptyImageURL: URL = URL(string: "https://www.swift-inc.com/public/images/images-empty.png")!
@@ -18,8 +18,6 @@ struct Constant {
 extension Constant {
     
     static func generateSearchURL(searchKey: String) -> URL? {
-        searchKey.isEmpty ?
-        URL(string: "") :
         URL(string: BASE_URL + QUERY_URL + searchKey)
     }
     
